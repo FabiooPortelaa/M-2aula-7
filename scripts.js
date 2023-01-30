90cabbc625d1b9e8d436095cb4f0a0b818695762
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-01-30 09:57:10 -0300

    alula 7

---
+++ scripts.js
@@ -0,0 +1,36 @@
+// let ListaDeCompras = ["ovos","leite","manteiga"] ;
+
+
+
+// let ListaDeCompras = [];
+// ListaDeCompras[0] = "ovos"
+// ListaDeCompras[1] = "manteiga"
+// ListaDeCompras[200] = "leite"
+
+
+// let ListaDeCompras = [];
+// ListaDeCompras.push("ovos");
+// ListaDeCompras.push("arroz");
+// ListaDeCompras.push("fejao");
+
+
+// let tentativas = 0;
+
+// function checartentativa(){
+// tentativas ++;
+// if(tentativas == 10){
+
+//     alert("Game Over");
+//     return
+// }
+// alert("tentativas");
+// }
+
+
+
+// function perdeu(){
+//     tentativas = 0;
+//     window.location.reload();
+// }
+
+
